406bfa5fb361fe8465f404daba9639ae1cce14b7
jwearnes <jwearnes@mtu.edu>
2011-06-15 22:53:25 -0400

    Cleaned up the Bitmaps example a bit.

--- examples/Bitmaps/Bitmaps.fs
+++ examples/Bitmaps/Bitmaps.fs
@@ -16,7 +16,6 @@
 :array sprite-tiles 81920 0 (320x256 pixel buffer)
 
 :include "../Sprites.fs"
-:include "../Print.fs"
 
 : pixel ( x y -- address )
 	2dup 64 / 5 * swap 64 / + 4096 *
@@ -25,7 +24,7 @@
 	ST @ +
 ;
 
-: init-sprites ( -- )
+: init-fullscreen ( -- )
 	19 for
 		64x64 i
 		i 5 mod 64 * # x
@@ -36,29 +35,18 @@
 
 : main
 
-	init-sprites
+	init-fullscreen
 
-	-350 # rolling animation counter
+	0 # rolling animation counter
 
 	loop
-
-		# build the iconic 'xor texture'
-		(
-		239 for
-			319 for
-				i 2 * j 2 * xor
-				dup dup 256 * or 256 * or 0xFF000000 or
-				i j pixel !
-			next
-		next
-		)
 
 		# Based on the example of figure 11 on page 4 of 
 		# The Art of Computer Programming, volume 4, fascile 1A.
 		239 for
 			319 for
 				dup dup i + j swap not over xor >r + 8 / r> and dup * 
-				255 mod dup dup 256 * or 256 * or 0xFF000000 or
+				dup dup 256 * or 256 * or 0xFF000000 or
 				i j pixel !
 			next
 		next
